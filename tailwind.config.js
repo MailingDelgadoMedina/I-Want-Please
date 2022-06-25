e1405495e35d58d1fe83d82e0635655a1f22d089
AlexandruD <AlexandruD@hotmail.com>
2022-06-25 23:49:08 +0300

    added the home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    typography: (theme) => ({}),
     extend: {},
   },
+  variants: {},
   plugins: [],
-}
+};
